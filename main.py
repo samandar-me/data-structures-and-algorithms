6f290402cf81db56d56db7ba6fb1d618a2afa6aa
samandar-me <samandarcoderdev@gmail.com>
2026-02-26 02:14:52 +0500

    Pascal triangle problem has been solved

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+def generate(n: int) -> list:
+    if n <= 0:
+        return []
+    triangle = [[1]]
+
+    for i in range(1, n):
+        previous_row = triangle[i - 1]
+        current_row = [1]
+        for j in range(0, len(previous_row) - 1):
+            sum = previous_row[j] + previous_row[j + 1]
+            current_row.append(sum)
+        current_row.insert(len(current_row), 1)
+        triangle.insert(i, current_row)
+    return triangle
+
 if __name__ == '__main__':
-    result = rotate([1,2,3,4,5,6,7], 3)
-    print(result)
+    print(generate(-1))
---
+++ problems/arrays/pascal_triangle.py
@@ -0,0 +1,8 @@
+# Find n lines from the beginning of Pascal's triangle.
+#
+# Input: 2
+# Result: [[1], [1,1]]
+#
+# Input: 5
+# Result: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
