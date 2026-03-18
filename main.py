12c855846370ae218f1c0048cd52a42c984480da
samandar-me <samandarcoderdev@gmail.com>
2026-03-18 15:32:37 +0500

    pulling rope is in progress

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+def leftRightDifference(nums: list) -> list:
+    result = []
 
+    for i in range(len(nums)):
+
+
+    return result
 
 if __name__ == '__main__':
-    print()
+    print(leftRightDifference([1, 2, 3, 4]))
