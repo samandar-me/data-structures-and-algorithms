bbfaceb44d2be1cc6a70b313eaed028a44a2766a
samandar-me <samandarcoderdev@gmail.com>
2026-04-05 21:28:00 +0500

    correct_brackets.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 def is_valid(s: str) -> bool:
+    counter = 0
+
+    for i in s:
+        if i == "(":
+            counter += 1
+        elif i == ")":
+            counter -= 1
+
+        if counter < 0:
+                return False
+
+    return counter == 0
 
 if __name__ == '__main__':
-    print(is_valid("((()))"))
+    print(is_valid(")"))
