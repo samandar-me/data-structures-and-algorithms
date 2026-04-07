a68ea162c579af14967f8bbbca6d7a6396e580fe
samandar-me <samandarcoderdev@gmail.com>
2026-04-08 00:12:49 +0500

    correct capitalization

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 def detectCapitalUse(word: str) -> bool:
+    if word.isupper() or word.islower():
+        return True
+
+    if not word:
+        return False
+
+    first_letter = word[0]
+    remaining_letters = word[1:len(word)]
+    return first_letter.isupper() and remaining_letters.islower()
 
 
 if __name__ == '__main__':
+    print(detectCapitalUse("FlaG"))
