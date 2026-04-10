da465eba4df059d1ec6af1824f0a1814c396d793
samandar-me <samandarcoderdev@gmail.com>
2026-04-10 16:49:12 +0500

    longest_palindrome.py in progress

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-
+def longestPalindrome(s: str) -> str:
+	pass
 
 if __name__ == '__main__':
+    print(longestPalindrome(""))
---
+++ string/longest_palindrome.py
@@ -0,0 +1,14 @@
+# Longest palindrome
+#
+# Find the longest palindrome in the given text. If there are 2 solutions ("bab", "aba") - return the first one ("bab").
+#
+# 💡 A palindrome is a word that is pronounced the same in reverse.
+#
+# Example 1:
+#
+# Input: "babad"
+# Result: "bobo"
+# Example 2:
+#
+# Input: "cbbd"
+# Result: "bb"
