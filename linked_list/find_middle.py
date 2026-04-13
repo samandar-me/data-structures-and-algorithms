59a27e4d35f4bef31f912faae4784d995a2e5079
samandar-me <samandarcoderdev@gmail.com>
2026-04-13 16:35:47 +0500

    the fist linked list problem is in progress

---
+++ linked_list/find_middle.py
@@ -0,0 +1,22 @@
+# Finding the middle of a Linked List
+#
+# Given the head, find and return the middle node of the Linked List.
+#
+# If the Linked List is of even length, return the middle node on the right.
+
+# Input: head = [1,2,3,4,5]
+# Result: [3,4,5]
+# Explanation: Since the node is returned, not the value in the middle, the answer 3 -> 4 -> 5 is correct.
+
+# Input: head = [1,2,3,4,5,6]
+# Result: [4,5,6]
+# Explanation: Node(4) was returned instead of Node(3) because the length is even.
+
+class Node:
+    def __init__(self, val=0):
+        self.val = val
+        self.next = None
+
+
+def middleNode(head: Node) -> Node:
+    pass
