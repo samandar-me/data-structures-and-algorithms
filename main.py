6f6e8ddcfed20240e6220373f51e3dcd5d041721
samandar-me <samandarcoderdev@gmail.com>
2026-04-16 23:41:04 +0500

    linked list

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+class Node:
+    def __init__(self, val=0):
+        self.val = val
+        self.next = None
 
+
+def middleNode(head: Node) -> Node:
+    return ""
 
 if __name__ == '__main__':
     print("")
