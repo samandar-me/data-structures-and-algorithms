a2d5a23375ddbd5bef853e77c87e21eab7e8825d
samandar-me <samandarcoderdev@gmail.com>
2026-04-17 00:17:22 +0500

    finding middle of linked list

--- main.py
+++ main.py
@@ -5,7 +5,22 @@ class Node:
 
 
 def middleNode(head: Node) -> Node:
-    return ""
+    slow = head
+    fast = head
+
+    while fast is not None and fast.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+
+    return slow
+
 
 if __name__ == '__main__':
-    print("")
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+
+    node1.next = node2
+    node2.next = node3
+
+    print(middleNode(node1).val)
