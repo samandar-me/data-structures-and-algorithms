d93e12e800ee18044ac862976bb57358456237c4
samandar-me <samandarcoderdev@gmail.com>
2026-04-20 01:01:51 +0500

    reverse linked list

--- linked_list/reverse_linked_list.py
+++ linked_list/reverse_linked_list.py
@@ -5,4 +5,28 @@ class Node:
 
 
 def reverse(head: Node) -> Node:
-    pass
+    previous = None
+    current = head
+
+    while current:
+        next_node = current.next
+        current.next = previous
+
+        previous = current
+        current = next_node
+
+    return previous
+
+if __name__ == '__main__':
+    n1 = Node(1)
+    n2 = Node(2)
+    n3 = Node(3)
+    n4 = Node(4)
+    n5 = Node(5)
+
+    n1.next = n2
+    n2.next = n3
+    n3.next = n4
+    n4.next = n5
+
+    print(reverse(n1).val)
