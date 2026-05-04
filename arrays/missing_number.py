5712d08906530b290da857536ae6d577917fa5bc
samandar-me <samandarcoderdev@gmail.com>
2026-05-05 00:09:06 +0500

    missing_number.py

---
+++ arrays/missing_number.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        sum1 = sum(nums)
+        sum2 = 0
+
+        for i in range(n+1):
+            sum2 += i
+
+        return sum2 - sum1
+
+    # Solution 1
+    # def missingNumber(self, nums: List[int]) -> int:
+    #     n = len(nums)
+    #     nums.sort()
+    #
+    #     if nums[0] != 0:
+    #         return 0
+    #
+    #     for i in range(len(nums) - 1):
+    #         sum = nums[i + 1] - nums[i]
+    #
+    #         if sum > 1:
+    #             return nums[i] + 1
+    #
+    #     return nums[n - 1] + 1
--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -1,6 +1,3 @@
-from ast import List
-
-
 from typing import List
 
 
