45d2ffc97992beac3ae89c57b7ef1a9aec18ff8f
samandar <samandarcoderdev@gmail.com>
2026-06-02 18:39:51 +0500

    invert binary tree in progress

--- main.py
+++ main.py
@@ -1,21 +1,74 @@
+from collections import deque
+
 class TreeNode:
     def __init__(self, val: int=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+def invertTree(root: TreeNode) -> TreeNode:
+    if not root:
+        return TreeNode()
+
+    new_node = root
+
+    queue = deque([root])
+
+    while queue:
+        node = queue.popleft()
+
+        print(node.val)
+
+        if node.left:
+            # new_node.right = node.left
+            queue.append(node.left)
+        if node.right:
+            # new_node.left = node.right
+            queue.append(node.right)
+
+    return TreeNode()
+
+
+def dfs(root: TreeNode) -> None:
+    if not root:
+        return
+
+    print(root.val)
+
+    dfs(root.left)
+    dfs(root.right)
+
+def bfs(root: TreeNode):
+    if not root:
+        return
+
+    queue = deque([root])
+
+    while queue:
+        node = queue.popleft()
+     #   print(node.val)
+
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
 
 if __name__ == '__main__':
-    t1 = TreeNode(3)
-    t2 = TreeNode(9)
-    t3 = TreeNode(20)
-    t4 = TreeNode(15)
-    t5 = TreeNode(7)
+    t4 = TreeNode(4)
+    t2 = TreeNode(2)
+    t1 = TreeNode(1)
+    t3 = TreeNode(3)
+    t7 = TreeNode(7)
+    t6 = TreeNode(6)
+    t9 = TreeNode(9)
 
-    t1.left = t2
-    t1.right = t3
+    t4.left = t2
+    t4.right = t7
 
-    t3.left = t4
-    t3.right = t5
+    t2.left = t1
+    t2.right = t3
 
-    print(count_leaves(t1))
+    t7.left = t6
+    t7.right = t9
+
+    print(invertTree(t4).val)
