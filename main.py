dae89eca835e13762637b87eb64ceb1954138b43
samandar <samandarcoderdev@gmail.com>
2026-08-12 00:29:53 +0500

    started stack problems

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from stack.asteroid_collision import Solution
+from stack.decode_string import Solution
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.asteroidCollision(asteroids = [3,5,-6,2,-1,4]))
+    print(s.decodeString(s="3[a2[c]]"))
