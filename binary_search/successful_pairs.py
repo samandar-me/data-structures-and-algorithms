216e07d52f895c4fe2d283db538c26120e017840
samandar <samandarcoderdev@gmail.com>
2026-09-19 10:48:15 +0500

    attempting on successful_pairs.py

---
+++ binary_search/successful_pairs.py
@@ -0,0 +1,33 @@
+class Solution:
+    def successfulPairs(self, spells: list[int], potions: list[int], success: int) -> list[int]:
+        potions.sort()
+        n = len(spells)
+        pn = len(potions)
+        pairs = [0] * n
+
+        for i in range(n):
+            count = self.binary_search_count(spells[i], pn, potions, success)
+            pairs[i] = count
+
+        return pairs
+
+    def binary_search_count(self, multiplier, pn, potions, success) -> int:
+       left = 0
+       right = pn - 1
+
+       while left <= right:
+           mid = left + ((right - left) // 2)
+           current = multiplier * potions[mid]
+           previous = multiplier * potions[mid - 1] if mid > 0 else 0
+           next = multiplier * potions[mid + 1] if mid < pn - 1 else 0
+
+           if current == success:
+               return pn - mid
+           if previous <= success <= next:
+               return pn - mid
+           elif current > success:
+               right = mid - 1
+           else:
+               left = mid + 1
+
+       return 0
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from binary_search.find_peak import Solution
+from binary_search.successful_pairs import Solution
 
 if __name__ == '__main__':
     s = Solution()
 
-    print(s.findPeakElement(nums = [1,2,3,1]))
+    print(s.successfulPairs([3,1,2], potions = [8,5,8], success = 16))
